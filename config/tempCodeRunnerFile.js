8b497bc97942e2f025f2460e3cec77f6a4c2045e
krishnakantnitp2025 <krishnav.ug21.ee@nitp.ac.in>
2024-05-25 09:34:25 +0530

    corrections made and added build folder

---
+++ config/tempCodeRunnerFile.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose'
+import colors from 'colors'
+import ConsoleHelperBackend from '../ConsoleHelperBackend'
+
+const connectDatabase=async()=>{
+    try {
+        const connect=await mongoose.connect(process.env.MONGO_URI)
+        ConsoleHelperBackend(`Connected to Mongo Database ${connect.connection.host}`.bgBlue.white)
+    } catch (error) {
+        ConsoleHelperBackend(`Error in MongoDB ${error}`.bgRed.white)
+    }
+}
+export default connectDatabase;
--- server.js
+++ server.js
@@ -7,6 +7,8 @@ import connectDatabase from "./config/database.js";
 import authRoutes from "./routes/authRoute.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
+import path from 'path'
+import ConsoleHelperBackend from "./ConsoleHelperBackend.js";
 
 dotenv.config();
 
@@ -18,6 +20,7 @@ app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, './client/build')));
 
 const BASE_URL=process.env.BASE_URL;
 
@@ -25,15 +28,18 @@ app.use(`/api/v1/auth`, authRoutes);
 app.use(`/api/v1/category`, categoryRoutes);
 app.use(`/api/v1/product`, productRoutes);
 
+// app.get("/", (req, res) => {
+//     res.send("<h1>Welcome To Nescafe NIT Patna</h1>");
+// });
 
-app.get("/", (req, res) => {
-    res.send("<h1>Welcome To Nescafe NIT Patna</h1>");
-});
+app.use('*', function(req, res){
+    res.sendFile(path.join(__dirname,'./client/build/index.html'))
+})
 
 const PORT = process.env.PORT || 8080;
 
 app.listen(`${PORT}`, () => {
-    console.log(
+    ConsoleHelperBackend(
         `Server is running on ${process.env.DEV} on port ${PORT}`.bgCyan.white
     );
 });
